9ef501a1f815f5df386df04354655cdc4a6260b0
Jeremiah Piontek <jwpiontek@gmail.com>
2016-05-05 21:25:38 -0500

    Basic functionality and tests done

--- goip.go
+++ goip.go
@@ -2,29 +2,46 @@ package goip
 
 import (
 	"encoding/json"
+	"errors"
 	"io/ioutil"
 	"net/http"
 )
 
+const IpApiUri = "http://ip-api.com/json/"
+
 type Location struct {
-	As          string `json:"as"`
-	City        string `json:"city"`
-	Country     string `json:"country"`
-	CountryCode string `json:"countryCode"`
-	Isp         string `json:"isp"`
-	Lat         string `json:"lat"`
-	Lon         string `json:"lon"`
-	Org         string `json:"org"`
-	Query       string `json:"query"`
-	Region      string `json:"region"`
-	RegionName  string `json:"regionName"`
-	Status      string `json:"status"`
-	Timezone    string `json:"timezone"`
-	Zip         string `json:"zip"`
+	As          string  `json:"as"`
+	City        string  `json:"city"`
+	Country     string  `json:"country"`
+	CountryCode string  `json:"countryCode"`
+	Isp         string  `json:"isp"`
+	Lat         float32 `json:"lat"`
+	Lon         float32 `json:"lon"`
+	Org         string  `json:"org"`
+	Query       string  `json:"query"`
+	Region      string  `json:"region"`
+	RegionName  string  `json:"regionName"`
+	Status      string  `json:"status"`
+	Timezone    string  `json:"timezone"`
+	Zip         string  `json:"zip"`
 }
 
-func GetLocation() (*Location, error) {
-	resp, err := http.Get("http://ip-api.com/json")
+type Client struct {
+	URI        string
+	HttpClient *http.Client
+}
+
+func (g *Client) GetLocation() (*Location, error) {
+	return getLocation(g.URI, g.HttpClient)
+}
+
+func (g *Client) GetLocationForIp(ip string) (*Location, error) {
+	uri := g.URI + ip
+	return getLocation(uri, g.HttpClient)
+}
+
+func getLocation(uri string, httpClient *http.Client) (*Location, error) {
+	resp, err := httpClient.Get(uri)
 	if err != nil {
 		return nil, err
 	}
@@ -37,5 +54,13 @@ func GetLocation() (*Location, error) {
 
 	var l Location
 	json.Unmarshal([]byte(contents), &l)
+	if l.Status != "success" {
+		err := errors.New("Failed to find location data")
+		return nil, err
+	}
 	return &l, nil
+}
+
+func NewClient() *Client {
+	return &Client{URI: IpApiUri, HttpClient: &http.Client{}}
 }
--- goip_test.go
+++ goip_test.go
@@ -1,14 +1,114 @@
 package goip
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
 	"testing"
 )
 
-func TestGetLocation(t *testing.T) {
-	result, err := GetLocation()
-	if err != nil {
-		t.Fatalf("Unexpected error", err)
+func getMockSuccessResponse() string {
+	l := Location{
+		As:          "500 North Pole",
+		City:        "Timbuktu",
+		Country:     "Fairy Tale Land",
+		CountryCode: "FT",
+		Isp:         "Acme ISP Service",
+		Lat:         55.555,
+		Lon:         33.333,
+		Org:         "Who Knows?",
+		Query:       "123.123.123.4",
+		Region:      "CM",
+		RegionName:  "Chocolate Mountain",
+		Status:      "success",
+		Timezone:    "America/Chicago",
+		Zip:         "12312",
 	}
-	fmt.Println(result.City)
+	result, _ := json.Marshal(l)
+	return string(result)
+}
+
+func getMockFailureResponse() string {
+	l := Location{
+		As:          "",
+		City:        "",
+		Country:     "",
+		CountryCode: "",
+		Isp:         "",
+		Lat:         0.0,
+		Lon:         0.0,
+		Org:         "",
+		Query:       "",
+		Region:      "",
+		RegionName:  "",
+		Status:      "fail",
+		Timezone:    "",
+		Zip:         "",
+	}
+	result, _ := json.Marshal(l)
+	return string(result)
+
+}
+
+func getMockServer(status int, responsePayload string) (*httptest.Server, *http.Client) {
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(status)
+		w.Header().Set("Content-Type", "application/json")
+		fmt.Fprintln(w, responsePayload)
+	}))
+
+	transport := &http.Transport{
+		Proxy: func(req *http.Request) (*url.URL, error) {
+			return url.Parse(server.URL)
+		},
+	}
+	httpClient := &http.Client{Transport: transport}
+
+	return server, httpClient
+}
+
+func TestGetLocationSuccess(t *testing.T) {
+	server, httpClient := getMockServer(200, getMockSuccessResponse())
+	client := Client{server.URL, httpClient}
+
+	location, err := client.GetLocation()
+	if err != nil {
+		t.Error(err)
+	}
+	if location.Status != "success" {
+		t.Error(errors.New("Expected success"))
+	}
+	if location.City != "Timbuktu" {
+		t.Error(errors.New("Expected Timbuktu"))
+	}
+}
+
+func TestGetLocationFailure(t *testing.T) {
+	// A failure response still returns a 200
+	server, httpClient := getMockServer(200, getMockFailureResponse())
+	client := Client{server.URL, httpClient}
+
+	location, err := client.GetLocation()
+	if location != nil {
+		t.Error("Location should be nil")
+	}
+	if err == nil {
+		t.Error("Should have returned an error")
+	}
+}
+
+func TestGetLocationHttpError(t *testing.T) {
+	server, httpClient := getMockServer(500, "")
+	client := Client{server.URL, httpClient}
+
+	location, err := client.GetLocation()
+	if location != nil {
+		t.Error("Location should be nil")
+	}
+	if err == nil {
+		t.Error("Should have returned an error")
+	}
 }
